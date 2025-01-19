fa0f38f4827161bfdcbc8fc2f03b94b277de5d58
Rajaram2004 <rajaramaa007@gmail.com>
2025-01-19 11:54:56 +0530

    update

--- assignments/Solutions/Recursion/print.java
+++ assignments/Solutions/Recursion/print.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;  
 public class print {
     public static void main(String[] args) {
-        System.out.println("test");
+       HashMap<Integer,Integer> mp=new HashMap<>();
+       mp.put(1,5);
+       mp.put(1,mp.get(1)+10);
+       mp.put(2,20);
+       System.out.println(mp);
+                                                                                      
     }
 }
